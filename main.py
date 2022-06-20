91d5541a18f5b07f9be7b21dd291a97c774633c8
Hassan <hassansultan77155@gmail.com>
2022-06-20 18:44:13 +0200

    test

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 # chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 # chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('window-size=1920x1080')
-chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument('window-size=1920x1080')
+# chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_driver_bath = "./chromedriver.exe"
 chrome_driver_bath = os.environ.get("CHROMEDRIVER_PATH")
 
