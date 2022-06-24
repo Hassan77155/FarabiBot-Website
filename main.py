80cf26c0daff5f220a693c95685a73067729be8c
Hassan <hassansultan77155@gmail.com>
2022-06-24 18:12:25 +0200

    Test fix

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ myu_URL = "http://193.227.50.64/"
 # This path depends on where is your chromedriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 # chrome_options.add_argument('--disable-gpu')
 # chrome_options.add_argument('window-size=1920x1080')
-chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_driver_bath = "./chromedriver.exe"
 chrome_driver_bath = os.environ.get("CHROMEDRIVER_PATH")
 
@@ -165,7 +165,7 @@ def main(user_id, user_password, rate):
     # Create a webdirver
     global driver
     # driver = webdriver.Chrome(executable_path=chrome_driver_bath)
-    driver = webdriver.Chrome(executable_path=chrome_driver_bath,chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=chrome_driver_bath,options=chrome_options)
     # Keep asking the user for input untill they cooperates
     # while True:
     #     try:
